fb24014c3ad5d2a9bfe3821d4f05c1ae9b1decb7
anagale27 <100821858+anagale27@users.noreply.github.com>
2023-10-30 21:14:20 -0400

    color with health impact

--- app.py
+++ app.py
@@ -28,14 +28,14 @@ def get_air_quality():
     city = request.form.get('city')
 
     if not city:
-        last_entered_city = request.cookies.get('lastEnteredCity', '')  # Retrieve last entered city
+        last_entered_city = request.cookies.get('lastEnteredCity', '')  # Retrieve the last entered city
         return render_template('index.html', air_quality="Please provide a city.", last_entered_city=last_entered_city)
 
-    air_quality, last_entered_city = get_air_quality_data(city)
+    air_quality, aqi_value, color, health_impact, caution = get_air_quality_data(city)
 
     # Store the last entered city in a cookie
-    response = make_response(render_template('index.html', air_quality=air_quality, last_entered_city=last_entered_city))
-    response.set_cookie('lastEnteredCity', last_entered_city)
+    response = make_response(render_template('index.html', air_quality=air_quality, last_entered_city=city, aqi_value=aqi_value, color=color, health_impact=health_impact, caution=caution))
+    response.set_cookie('lastEnteredCity', city)
 
     return response
 
@@ -57,12 +57,41 @@ def get_air_quality_data(city):
         data = response.json()
         if data['results']:
             value = data['results'][0]['value']
-            air_quality = f"The PM2.5 air quality in {city} is {value} µg/m³."
-            return air_quality, city
+            air_quality, color, health_impact, caution = get_aqi_info(value)  # Pass the AQI value to get_aqi_info
+            return air_quality, value, color, health_impact, caution
         else:
-            return f"No air quality data found for {city}.", city
+            return f"No air quality data found for {city}.", 0, 'gray', '', ''
     else:
-        return f"Error fetching air quality data: {response.status_code}", city
+        return f"Error fetching air quality data: {response.status_code}", 0, 'gray', '', ''
+
+def get_aqi_info(aqi_value):
+    if aqi_value <= 50:
+        color = 'green'
+        health_impact = 'Good'
+        caution = 'Air quality is satisfactory, and air pollution poses little or no risk.'
+    elif aqi_value <= 100:
+        color = 'yellow'
+        health_impact = 'Moderate'
+        caution = 'Air quality is acceptable; however, some pollutants may be a concern for a small number of people who are unusually sensitive to air pollution.'
+    elif aqi_value <= 150:
+        color = 'orange'
+        health_impact = 'Unhealthy for Sensitive Groups'
+        caution = 'Members of sensitive groups may experience health effects, but the general public is less likely to be affected.'
+    elif aqi_value <= 200:
+        color = 'red'
+        health_impact = 'Unhealthy'
+        caution = 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.'
+    elif aqi_value <= 300:
+        color = 'purple'
+        health_impact = 'Very Unhealthy'
+        caution = 'Health alert: everyone may experience more serious health effects.'
+    else:
+        color = 'maroon'
+        health_impact = 'Hazardous'
+        caution = 'Health warning of emergency conditions: the entire population is more likely to be affected.'
+
+    air_quality = f"The PM2.5 air quality in this area is {aqi_value} µg/m³."
+    return air_quality, color, health_impact, caution
 
 if __name__ == '__main__':
     app.run(debug=True)
